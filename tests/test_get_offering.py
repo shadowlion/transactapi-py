8621e5a7a05b26bad8c2723a62f8cefcf60ae116
Justin Chiou <chiou.kai@gmail.com>
2022-10-14 12:50:34 -0400

    refactor: successful data

--- tests/test_get_offering.py
+++ tests/test_get_offering.py
@@ -3,34 +3,34 @@ import pytest
 from transact_api import TransactApiClient
 from transact_api.endpoints.get_offering import GetOfferingResponse
 
+example_data_successful = {
+    "statusCode": "101",
+    "statusDesc": "Ok",
+    "offeringDetails": [
+        {
+            "issuerId": "96763",
+            "offeringId": "59249",
+            "issueName": "NC Offer",
+            "issueType": "debt",
+            "targetAmount": "20000.000000",
+            "minAmount": "100.000000",
+            "maxAmount": "25000.000000",
+            "unitPrice": "10.000000",
+            "totalShares": "2500.000000",
+            "remainingShares": "2500.000000",
+            "startDate": "10-17-2016",
+            "endDate": "12-31-2016",
+            "offeringStatus": "Pending",
+            "offeringText": "Test",
+            "stampingText": None,
+        },
+    ],
+}
+
 
 @pytest.fixture
 def mocked_response(mocker):
-    example_response = GetOfferingResponse(
-        **{
-            "statusCode": "101",
-            "statusDesc": "Ok",
-            "offeringDetails": [
-                {
-                    "issuerId": "96763",
-                    "offeringId": "59249",
-                    "issueName": "NC Offer",
-                    "issueType": "debt",
-                    "targetAmount": "20000.000000",
-                    "minAmount": "100.000000",
-                    "maxAmount": "25000.000000",
-                    "unitPrice": "10.000000",
-                    "totalShares": "2500.000000",
-                    "remainingShares": "2500.000000",
-                    "startDate": "10-17-2016",
-                    "endDate": "12-31-2016",
-                    "offeringStatus": "Pending",
-                    "offeringText": "Test",
-                    "stampingText": None,
-                },
-            ],
-        }
-    )
+    example_response = GetOfferingResponse(**example_data_successful)
     mocker.patch(
         "transact_api.TransactApiClient.get_offering",
         return_value=example_response,
