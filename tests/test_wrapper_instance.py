bd5a45514de2fdf31d2835b92b955acc187e3b60
Justin Chiou <chiou.kai@gmail.com>
2022-12-09 11:11:06 -0500

    refactor: test instance more thoroughly

--- tests/test_wrapper_instance.py
+++ tests/test_wrapper_instance.py
@@ -9,9 +9,36 @@ def test_class_instantiation_no_key():
         TransactApiClient()
 
 
-def test_class_instantiation_with_keys():
-    client = TransactApiClient(client_id="asdf", developer_api_key="qwer")
+def test_client_instance_with_default_sandbox_behavior():
+    client = TransactApiClient(
+        client_id="asdf",
+        developer_api_key="qwer",
+    )
     assert isinstance(client, TransactApiClient)
     assert client.client_id == "asdf"
     assert client.developer_api_key == "qwer"
     assert not client.sandbox
+
+
+def test_class_instantiation_set_sandbox_to_FALSE():
+    client = TransactApiClient(
+        client_id="asdf",
+        developer_api_key="qwer",
+        sandbox=False,
+    )
+    assert isinstance(client, TransactApiClient)
+    assert client.client_id == "asdf"
+    assert client.developer_api_key == "qwer"
+    assert not client.sandbox
+
+
+def test_class_instantiation_set_sandbox_to_TRUE():
+    client = TransactApiClient(
+        client_id="asdf",
+        developer_api_key="qwer",
+        sandbox=True,
+    )
+    assert isinstance(client, TransactApiClient)
+    assert client.client_id == "asdf"
+    assert client.developer_api_key == "qwer"
+    assert client.sandbox
